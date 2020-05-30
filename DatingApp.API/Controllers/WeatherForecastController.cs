6ecff5ea20145aacdb0ca065b5092ead2d4d28f9
zakariabijoy <zakaria.bijoy@live.com>
2020-05-31 03:30:41 +0600

    Initial Commit (an asp.net core web api (for api service )and an angular app (for client-spa) project is created, code first approch is implemented to aasp.net web api project using ef core, implemeted functionality(like cors) for communicating client-spa(angular) with web api(asp.net core) project through httpclient request )

---
+++ DatingApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ApplicationDbContext context)
+        {
+            this._context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+
+            // var rng = new Random();
+            // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            // {
+            //     Date = DateTime.Now.AddDays(index),
+            //     TemperatureC = rng.Next(-20, 55),
+            //     Summary = Summaries[rng.Next(Summaries.Length)]
+            // })
+            // .ToArray();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id){
+            var value = await _context.Values.FirstOrDefaultAsync(value=>value.Id == id);
+            return Ok(value);
+        }
+
+    }
+}
