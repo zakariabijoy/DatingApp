131889bb73dead037e45354e88e3b0ec3803b856
zakariabijoy <zakaria.bijoy@live.com>
2020-06-03 03:57:22 +0600

    implement angular template form to login form with using angular disable attribute to login button if form is not valid in SPA

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -14,7 +16,8 @@ import { NavComponent } from './nav/nav.component';
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
